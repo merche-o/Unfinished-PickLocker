49305e5d9665573f865799b0cedc03c14cdb57bd
Ainur <o.mercher24@gmail.Com>
2015-10-08 14:18:21 +0200

    heroku push

---
+++ iOS/PickLocker/PickLocker/HomePageTabBarController.swift
@@ -0,0 +1,25 @@
+//
+//  HomePageTabBarController.swift
+//  PickLocker
+//
+//  Created by Ainur on 08/10/2015.
+//  Copyright © 2015 A&D. All rights reserved.
+//
+
+import UIKit
+
+class HomePageTabBarController: UITabBarController {
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+}
---
+++ iOS/PickLocker/PickLocker/HomePageViewController.swift
@@ -0,0 +1,26 @@
+//
+//  HomePageViewController.swift
+//  PickLocker
+//
+//  Created by Ainur on 08/10/2015.
+//  Copyright © 2015 A&D. All rights reserved.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+       // playButton.addT
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
